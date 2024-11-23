ef237997e98815895f77cf3b69be7273a3be311f
roei levi <roei1234567@gmail.com>
2024-11-23 18:35:48 +0200

    0105

--- exercises/01_structured_logging/05_fields/src/lib.rs
+++ exercises/01_structured_logging/05_fields/src/lib.rs
@@ -1,6 +1,7 @@
 mod subscriber;
 
 pub use subscriber::init_test_subscriber;
+use tracing::{error, event, field, info, span, Level};
 
 /// Given a list of order numbers, compute the total price.
 ///
@@ -11,11 +12,20 @@ pub use subscriber::init_test_subscriber;
 ///
 /// Refer to the test files for the expected output format.
 pub fn get_total(order_numbers: &[u64]) -> Result<u64, anyhow::Error> {
+    let span = span!(Level::INFO, "process total price", outcome = field::Empty).entered();
+
     let mut total = 0;
     for order_number in order_numbers {
-        let order_details = get_order_details(*order_number)?;
-        total += order_details.price;
+        match get_order_details(*order_number) {
+            Ok(order_details) => total += order_details.price,
+            Err(e) => {
+                span.record("outcome", "failure");
+                anyhow::bail!(e)
+            }
+        }
     }
+
+    span.record("outcome", "success");
     Ok(total)
 }
 
@@ -26,10 +36,14 @@ pub struct OrderDetails {
 
 /// A dummy function to simulate what would normally be a database query.
 fn get_order_details(order_number: u64) -> Result<OrderDetails, anyhow::Error> {
+    let span = span!(Level::INFO, "retrieve order", outcome = field::Empty).entered();
+
     if order_number % 4 == 0 {
+        span.record("outcome", "failure");
         Err(anyhow::anyhow!("Failed to talk to the database"))
     } else {
         let prices = vec![999, 1089, 1029];
+        span.record("outcome", "success");
         Ok(OrderDetails {
             order_number,
             price: prices[order_number as usize % prices.len()],
