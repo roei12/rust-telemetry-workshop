47e6ae3171734fa4d45d970bff57f4b3fc839949
roei levi <roei1234567@gmail.com>
2024-11-23 19:09:04 +0200

    0107

--- exercises/01_structured_logging/07_structured_output/src/subscriber.rs
+++ exercises/01_structured_logging/07_structured_output/src/subscriber.rs
@@ -7,6 +7,8 @@ pub fn init_test_subscriber() -> MockWriter {
     tracing_subscriber::fmt()
         .with_writer(move || writer.clone())
         .with_span_events(FmtSpan::NEW | FmtSpan::EXIT)
+        .json()
+        .flatten_event(true)
         // TODO: we want JSON! Check out the test suite to understand the expected output.
         // Make sure to **flatten your events**!
         .init();
