94fc175c2adc48b60a71352ba44fa48013b48b96
roei levi <roei1234567@gmail.com>
2024-11-23 14:32:30 +0200

    0103

--- exercises/01_structured_logging/03_what_to_log/src/lib.rs
+++ exercises/01_structured_logging/03_what_to_log/src/lib.rs
@@ -1,5 +1,7 @@
 mod logger;
 
+use std::time::Instant;
+
 pub use logger::TestLogger;
 
 /// Given a list of order numbers, compute the total price.
@@ -14,7 +16,28 @@ pub use logger::TestLogger;
 ///
 /// Refer to the test files for the expected output format.
 pub fn get_total(order_numbers: &[u64]) -> Result<u64, anyhow::Error> {
-    todo!()
+    log::info!("START - process total price");
+    let starting_time = Instant::now();
+    let mut sum = 0u64;
+
+    for order in order_numbers {
+        match get_order_details(*order) {
+            Ok(details) => sum += details.price,
+            Err(e) => {
+                log::error!(
+                    "END - process total price - ERROR - {}ms",
+                    starting_time.elapsed().as_millis()
+                );
+                anyhow::bail!(e);
+            }
+        }
+    }
+
+    log::info!(
+        "END - process total price - SUCCESS - {}ms",
+        starting_time.elapsed().as_millis()
+    );
+    Ok(sum)
 }
 
 pub struct OrderDetails {
@@ -24,13 +47,27 @@ pub struct OrderDetails {
 
 /// A dummy function to simulate what would normally be a database query.
 fn get_order_details(order_number: u64) -> Result<OrderDetails, anyhow::Error> {
-    if order_number % 4 == 0 {
+    log::info!("START - retrieve order");
+    let starting_time = Instant::now();
+    let prices = vec![999, 1089, 1029];
+
+    let result = if order_number % 4 == 0 {
+        log::error!(
+            "END - retrieve order - ERROR - {}ms",
+            starting_time.elapsed().as_millis()
+        );
         Err(anyhow::anyhow!("Failed to talk to the database"))
     } else {
-        let prices = vec![999, 1089, 1029];
+        let price = prices[order_number as usize % prices.len()];
+        log::info!(
+            "END - retrieve order - SUCCESS - {}ms",
+            starting_time.elapsed().as_millis()
+        );
         Ok(OrderDetails {
             order_number,
-            price: prices[order_number as usize % prices.len()],
+            price,
         })
-    }
+    };
+
+    result
 }
